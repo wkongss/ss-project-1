80d9b77d6ebcb8bfe8b31816995d25c8e7559be7
Wilson Kong <wkong@skillstorm.com>
2026-02-04 09:06:07 -0500

    Finished adding all routes

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -3,6 +3,24 @@ import ProductController from "../controllers/product.controller.js";
 
 const router = Router();
 
-router.route("");
+router.route("/")
+    .get((req, res) => {
+        ProductController.getAllProducts(req, res);
+    })
+    .post((req, res) => {
+        ProductController.createProduct(req, res);
+    })
+    .put((req, res) => {
+        ProductController.updateProduct(req, res);
+    })
+
+router.route("/:id")
+    .get((req, res) => {
+        ProductController.getProductById(req, res);
+    })
+    .delete((req, res) => {
+        ProductController.deleteProduct(req, res);
+    });
+
 
 export default router;
