f2a5aa15d54248ff906201cd156d18e3022e43ef
Wilson Kong <wkong@skillstorm.com>
2026-02-04 09:14:40 -0500

    Finished basic CRUD repos + documentation

--- backend/repos/product.repo.js
+++ backend/repos/product.repo.js
@@ -1,34 +1,53 @@
 import Product from "../models/product.model.js";
-import { Document, Query } from "mongoose";
 
 /**
  * Finds and returns all products from the database
- * @returns {Query<Product>} A promise with all products
+ * @returns {Promise<Product[]>} A promise with all products
  */
 function findAll() {
-    return Product.find();
+    return Product.find().exec();
+}
+
+/**
+ * Finds and returns a product by its ID
+ * @param {string} _id The id of the product to find
+ * @returns {Promise<Product | null>} A promise of the document of the product found
+ */
+function findProductById(_id) {
+    return Product.findById(_id).exec();
 }
 
 /**
  * Creates a product with the given data
  * @param {{ sku: string, name: string, description?: string }} data 
- * @returns {Promise<Document>} the document created
+ * @returns {Promise<Product>} A promise of the document created
  */
 function createProduct(data) {
-    return Product.create(data);
+    return Product.create(data).exec();
 }
 
 /**
+ * Updates a product with given data
+ * @param {string} _id The id of the product to update
+ * @param {{ sku: string, name: string, description?: string }} newData 
+ * @returns {Promise<Product>} A promise with the updated unit
+ */
+async function updateProduct(_id, newData) {
+    return Product.findByIdAndUpdate(_id, newData, { returnDocument: "after" }).exec();
+}
+
+
+/**
  * Deletes the product with the given ID.
- * @param {string} _id the ObjectId of the product to delete 
- * @returns {Promise<Document>} the document deleted (if available)
+ * @param {string} _id The id of the product to delete 
+ * @returns {Promise<Product>} A promise of the document deleted (if available)
  */
 function deleteProduct(_id) {
-    return Product.findOneAndDelete(_id);
+    return Product.findOneAndDelete(_id).exec();
 }
 
 const ProductRepo = {
-    findAll, createProduct, deleteProduct
+    findAll, findProductById, createProduct, updateProduct, deleteProduct
 };
 
 export default ProductRepo;
