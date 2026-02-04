ada0fe26bd934879d03dd95790ae36557aa435c0
Wilson Kong <wkong@skillstorm.com>
2026-02-04 09:09:28 -0500

    Finished controllers with documentation

--- backend/controllers/activity.controller.js
+++ backend/controllers/activity.controller.js
@@ -1,11 +1,17 @@
 import ActivityService from "../services/activity.service.js";
 
+/**
+ * Handles GET /api/v1/activities/
+ */
 async function getAll(_, res) {
     const data = await ActivityService.findAll();
 
     return res.status(200).json(data);
 }
 
+/**
+ * Handles GET /api/v1/activities/:limit
+ */
 async function getMostRecent(req, res) {
     const { limit } = req.params;
     const data = ActivityService.findMostRecent(limit);
--- backend/controllers/unit.controller.js
+++ backend/controllers/unit.controller.js
@@ -1,35 +1,100 @@
 import UnitService from "../services/unit.service.js";
 
-async function get(req, res) {
-    const { warehouse, product } = req.query;
+/**
+ * Handles GET /api/v1/units/
+ */
+async function getAllUnits(req, res) {
+    try {
+        const { warehouse, product } = req.query;
+        let data;
 
-    if (warehouse) {
-        return 
+        if (warehouse) {
+            data = await UnitService.findUnitsByWarehouse(warehouse);
+        } else if (product) {
+            data = await UnitService.findUnitsByProduct(product);
+        } else {
+            data = await UnitService.findAll();
+            return res.status(200).json(data);
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send("Something went wrong!");
     }
 }
 
-async function getAllByWarehouse() {
+/**
+ * Handles GET /api/v1/units/:id
+ */
+async function getUnitById(req, res) {
+    try {
+        const { id } = req.params;
+        const data = await UnitService.findUnitById(id);
 
+        if (!data) {
+            return res.sendStatus(404);
+        }
+
+        return res.status(200).json(data);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send("Something went wrong!");
+    }
 }
 
-async function getAllByProduct() {
+/**
+ * Handles POST /api/v1/units/
+ */
+async function createUnit(req, res) {
+    try {
+        const data = req.body;
 
+        const document = await UnitService.createUnit(data);
+        res.status(201).json(document);
+    } catch (error) {
+        if (error instanceof RangeError) {
+            return res.status(400).send(error.message);
+        }
+
+        console.error(error);
+        return res.status(500).send("Something went wrong!");
+    }
 }
 
-async function createUnit() {
+/**
+ * Handles PUT /api/v1/units/
+ */
+async function updateUnit(req, res) {
+    try {
+        const data = req.body;
+        const document = await UnitService.updateUnit(data);
+        res.status(201).json(document);
+    } catch (error) {
+        if (error instanceof RangeError) {
+            return res.status(400).send(error.message);
+        }
 
+        console.error(error);
+        return res.status(500).send("Something went wrong!");
+    }
 }
 
-async function updateUnit() {
+/**
+ * Handles DELETE /api/v1/units/:id
+ */
+async function deleteUnit(req, res) {
+    try {
+        const { id } = req.params;
 
-}
-
-async function deleteUnit() {
-
+        await UnitService.deleteUnit(id);
+        res.sendStatus(204);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send("Something went wrong!");
+    }
 }
 
 const UnitController = {
-
+    getAllUnits, getUnitById, createUnit, updateUnit, deleteUnit
 };
 
 export default UnitController;
