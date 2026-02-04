5754c9544b6ee9a8f580fb1b0e1f7dfa9ac5f416
Wilson Kong <wkong@skillstorm.com>
2026-02-04 09:22:12 -0500

    Added all services + documentation

--- backend/services/activity.service.js
+++ backend/services/activity.service.js
@@ -1,9 +1,19 @@
 import ActivityRepo from "../repos/activity.repo.js";
+import Activity from "../models/activity.model.js";
 
+/**
+ * Gets all activities
+ * @returns {Promise<Activity[]>} A promise containing activity data
+ */
 async function findAll() {
     return await ActivityRepo.findAll();
 }
 
+/**
+ * Gets the most recent activities, sorted by newest to oldest
+ * @param {number} limit The number of activities to fetch
+ * @returns {Promise<Activity[]>} A promise containing activity data
+ */
 async function findMostRecent(limit) {
     return await ActivityRepo.findMostRecent(limit);
 }
