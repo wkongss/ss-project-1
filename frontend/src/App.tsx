ea13dacc6752239d210bcdb36efeefe8e3cc7e02
Wilson Kong <wkong@skillstorm.com>
2026-02-05 12:47:38 -0500

    Added outlet for site layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router";
+import Layout from "./components/layouts/Layout";
 
-function App() {
-  const [count, setCount] = useState(0)
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    {/* <Route index element={<Dashboard />} /> */}
+
+                    <Route path="warehouses" element={<p>A</p> } >
+                        {/* <Route index element={<WarehousePage />} />
+                        <Route path=":id" element={<UnitPage />} /> */}
+                    </Route>
+
+                    <Route path="catalog" element={<p>B</p> } >
+                        {/* <Route index element={<WarehousePage />} />
+                        <Route path=":id" element={<UnitPage />} /> */}
+                    </Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
-
-export default App
